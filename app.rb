d9b1e527a81cf6e6f627722d0b81ece6a5de5dd3
Alina Sapronova <106851105+AlinaSapronova@users.noreply.github.com>
2023-08-03 02:25:53 +0000

    created flexable file and modified dynamic path

--- app.rb
+++ app.rb
@@ -52,3 +52,52 @@ get("/dice/5/4") do
 
   erb(:five_four)
 end
+
+get("/dynamic/6/19") do
+  @rolls = []
+
+  6.times do
+    die = rand(1..19)
+
+    @rolls.push(die)
+  end
+
+  erb(:six_ninteen)
+end
+
+get("/dynamic/20/4") do
+  @rolls = []
+
+  20.times do
+    die = rand(1..4)
+
+    @rolls.push(die)
+  end
+
+  erb(:twenty_four)
+end
+
+get("/dynamic/42/1337") do
+  @rolls = []
+
+  42.times do
+    die = rand(1..1337)
+
+    @rolls.push(die)
+  end
+
+  erb(:forty_two)
+end
+
+get("/dynamic/:number_of_dice/6") do
+  @num_dice = params.fetch("number_of_dice").to_i
+  @rolls = []
+
+  @num_dice.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end
+
+  erb(:flexible)
+end
